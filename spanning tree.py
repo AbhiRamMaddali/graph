dc9bede0eef316a4f47322fb9976fc9c02dd5f07
AbhiRamMaddali <abhiram.maddali1204@gmail.com>
2025-05-11 16:59:25 +0530

    Update spanning tree.py

--- spanning tree.py
+++ spanning tree.py
@@ -1,1 +1,12 @@
+#it is only for undirected graph
+#In General  "spanning" means:Covering all the parts of something.
+#In the case of a spanning tree:
+#Spanning" means the tree reaches (covers) all the vertices of the graph.
+#You're not skipping any node — every node is included and connected.
+#The basic idea of a Spanning Tree is:A spanning tree is a subgraph of a connected graph that includes all the vertices and 
+#forms a tree — meaning no cycles and the minimum number of edges needed to keep all nodes connected.
+
+#There are two ways to find minimum spanning tree:
+#Prim's Algorithm
+#Kruskal's Algorithm
 
